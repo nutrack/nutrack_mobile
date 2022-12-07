dd1522d9744d5bc42db65ae04b072eca0646f772
sashanabilafortuna <sashanabilafortuna@gmail.com>
2022-12-07 14:51:31 +0700

    add drawer

---
+++ lib/model/model_drawer.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class NutrackDrawerItem {
+  final String title;
+  final IconData icon;
+
+  const NutrackDrawerItem({
+    required this.title,
+    required this.icon,
+  });
+}
